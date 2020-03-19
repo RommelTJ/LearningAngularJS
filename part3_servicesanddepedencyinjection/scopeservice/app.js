082b269a728d8ff358c482ab8d33eb51cea8bb0e
Rommel Rico <me@rommelrico.com>
2020-03-19 08:42:26 -1000

    :sparkles: ScopeService: Adding a function to the scope

--- part3_servicesanddepedencyinjection/scopeservice/app.js
+++ part3_servicesanddepedencyinjection/scopeservice/app.js
@@ -4,5 +4,8 @@ myApp.controller('mainController', function($scope) {
   // AngularJS Dependency Injection includes $scope object service
   // All AngularJS services start with a dollar sign.
   $scope.name = "Rommel";
+  $scope.getName = function() {
+    return 'John Doe';
+  };
   console.log($scope);
 });
