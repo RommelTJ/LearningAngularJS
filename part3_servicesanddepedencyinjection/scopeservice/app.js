ffe726d168f1d182a120a88b7416232cf0dc2639
Rommel Rico <me@rommelrico.com>
2020-03-19 08:40:00 -1000

    :sparkles: ScopeService: Logging the scope service

--- part3_servicesanddepedencyinjection/scopeservice/app.js
+++ part3_servicesanddepedencyinjection/scopeservice/app.js
@@ -1,5 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function() {
-    
+myApp.controller('mainController', function($scope) {
+  console.log($scope); // AngularJS Dependency Injection includes $scope object service
+  // All AngularJS services start with a dollar sign.
 });
