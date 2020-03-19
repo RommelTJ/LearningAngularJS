318927dec329816a61bd493df97614cde516a590
Rommel Rico <me@rommelrico.com>
2020-03-19 08:41:07 -1000

    :sparkles: ScopeService: Adding a variable to the scope

--- part3_servicesanddepedencyinjection/scopeservice/app.js
+++ part3_servicesanddepedencyinjection/scopeservice/app.js
@@ -1,6 +1,8 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function($scope) {
-  console.log($scope); // AngularJS Dependency Injection includes $scope object service
+  // AngularJS Dependency Injection includes $scope object service
   // All AngularJS services start with a dollar sign.
+  $scope.name = "Rommel";
+  console.log($scope);
 });
