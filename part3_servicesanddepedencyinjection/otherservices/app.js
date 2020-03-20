749fcf74c130d0d85d1d8136e3b8384f5ba9ce65
Rommel Rico <me@rommelrico.com>
2020-03-20 10:16:02 -1000

    :sparkles: Services: Using the $filter

--- part3_servicesanddepedencyinjection/otherservices/app.js
+++ part3_servicesanddepedencyinjection/otherservices/app.js
@@ -2,7 +2,10 @@ var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
 myApp.controller('mainController', function($scope, $log, $filter, $resource) {
 
-    console.log($filter);
-    console.log($resource);
+    $scope.name = "John";
+    $scope.formattedName = $filter("uppercase")($scope.name);
+
+    $log.info(`Name: ${$scope.name}`);
+    $log.info(`Formatted Name: ${$scope.formattedName}`);
     
 });
