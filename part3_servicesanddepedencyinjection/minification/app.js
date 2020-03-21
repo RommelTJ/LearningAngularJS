918fd598af3cc5bd92a35b5c55bad5b66852f5ca
Rommel Rico <me@rommelrico.com>
2020-03-21 08:56:00 -1000

    :sparkles: Minification: Showing normal version

--- part3_servicesanddepedencyinjection/minification/app.js
+++ part3_servicesanddepedencyinjection/minification/app.js
@@ -1,8 +1,5 @@
-// var myApp = angular.module('myApp', []);
-//
-// myApp.controller('mainController', ["$scope", "$log", function($scope, $log) {
-//   $log.info($scope);
-// }]);
+var myApp = angular.module('myApp', []);
 
-var myApp=angular.module('myApp',[]);
-myApp.controller('mainController',["$scope","$log",function($scope,$log){$log.info($scope)}]);
+myApp.controller('mainController', ["$scope", "$log", function($scope, $log) {
+  $log.info($scope);
+}]);
