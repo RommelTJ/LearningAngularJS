62745dfbb11595a46144c44d6ff1c5006ec78496
Rommel Rico <me@rommelrico.com>
2020-03-24 09:45:38 -0700

    :sparkles: CommonDirectives: ng-repeat

--- part4_databindingsanddirectives/commondirectives/app.js
+++ part4_databindingsanddirectives/commondirectives/app.js
@@ -9,5 +9,11 @@ myApp.controller('mainController', ['$scope', '$filter', function($scope, $filte
     };
 
     $scope.characters = 5;
+
+    $scope.rules = [
+        { rulename: "Must be 5 characters" },
+        { rulename: "Must not be used elsewhere" },
+        { rulename: "Must be cool" }
+    ];
     
 }]);
