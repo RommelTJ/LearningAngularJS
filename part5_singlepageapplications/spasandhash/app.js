9091e21911ddebeba7ed9315dabb75d4777efd8c
Rommel Rico <me@rommelrico.com>
2020-03-30 07:06:16 -0700

    :sparkles: SPAs: Handling specific hash

--- part5_singlepageapplications/spasandhash/app.js
+++ part5_singlepageapplications/spasandhash/app.js
@@ -1,3 +1,5 @@
 window.addEventListener('hashchange', function() {
-  console.log("Hash changed!: " + window.location.hash);
+  if (window.location.hash === "#/bookmark/1") {
+    console.log("Page 1 is cool.");
+  }
 });
