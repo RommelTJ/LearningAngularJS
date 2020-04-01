362f6d592d7aca7f95d58f8477e968b1b3c66898
Rommel Rico <me@rommelrico.com>
2020-04-01 07:21:45 -0700

    :sparkles: SPAs: Using $routeProvider

--- part5_singlepageapplications/routingtemplatesandcontrollers/app.js
+++ part5_singlepageapplications/routingtemplatesandcontrollers/app.js
@@ -1,7 +1,15 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function($routeProvider) {
-  
+  $routeProvider
+    .when('/', {
+      templateUrl: '../pages/main.html',
+      controller: 'mainController'
+    })
+    .when('/second', {
+      templateUrl: '../pages/second.html',
+      controller: 'mainController'
+    });
 });
 
 myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
