2f942c33bf5a2504e3cf8b4bce1504a452d7a389
Rommel Rico <me@rommelrico.com>
2020-04-01 07:18:54 -0700

    :sparkles: SPAs: Configuring ngRoute with $routeProvider

--- part5_singlepageapplications/routingtemplatesandcontrollers/app.js
+++ part5_singlepageapplications/routingtemplatesandcontrollers/app.js
@@ -1,5 +1,9 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
+myApp.config(function($routeProvider) {
+  
+});
+
 myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
     
 
