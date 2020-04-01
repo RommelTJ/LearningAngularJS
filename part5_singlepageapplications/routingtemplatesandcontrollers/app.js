0e21d77a127f725062b66af0e026881f16643d49
Rommel Rico <me@rommelrico.com>
2020-04-01 07:13:11 -0700

    :sparkles: SPAs: Logging the hash path

--- part5_singlepageapplications/routingtemplatesandcontrollers/app.js
+++ part5_singlepageapplications/routingtemplatesandcontrollers/app.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$location', function($scope, $location) {
+myApp.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
     
-  
+  $log.info($location.path());
     
 }]);
