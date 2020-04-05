7845c2c9a3805e0f59f7d9023e670a0e2188e589
Rommel Rico <me@rommelrico.com>
2020-04-04 20:56:29 -0700

    :art: CustomDirectives: Spacing

--- part7_customdirectives/app.js
+++ part7_customdirectives/app.js
@@ -2,32 +2,28 @@ var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider, $locationProvider) {
 
-    $locationProvider.hashPrefix('');
-    $routeProvider
-      .when('/', {
-        templateUrl: 'pages/main.html',
-        controller: 'mainController'
-      })
-      .when('/second', {
-        templateUrl: 'pages/second.html',
-        controller: 'secondController'
-      })
-      .when('/second/:num', {
-        templateUrl: 'pages/second.html',
-        controller: 'secondController'
-      });
+  $locationProvider.hashPrefix('');
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/main.html',
+      controller: 'mainController'
+    })
+    .when('/second', {
+      templateUrl: 'pages/second.html',
+      controller: 'secondController'
+    })
+    .when('/second/:num', {
+      templateUrl: 'pages/second.html',
+      controller: 'secondController'
+    });
 });
 
 myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
-    
-    
-    
+
 }]);
 
 myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
-    
-    
-    
+
 }]);
 
 myApp.directive("searchResults", function() {
