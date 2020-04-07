b196abbd5deecafc902e2d8eb9d05eed4387cb99
Rommel Rico <me@rommelrico.com>
2020-04-07 06:57:40 -0700

    :sparkles: WeatherApp: Setting up home and forecast page

--- part8_app/app.js
+++ part8_app/app.js
@@ -1,2 +1,22 @@
 var weatherApp = angular.module('weatherApp', ["ngRoute", "ngResource"]);
 
+weatherApp.config(function($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('');
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/home.html',
+      controller: 'homeController'
+    })
+    .when('/forecast', {
+      templateUrl: 'pages/forecast.html',
+      controller: 'forecastController'
+    });
+});
+
+weatherApp.controller('homeController', ['$scope', function($scope) {
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function ($scope) {
+
+}]);
