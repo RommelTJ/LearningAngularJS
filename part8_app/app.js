95b041a16e93d0f1e74bc747bd167ad148f352df
Rommel Rico <me@rommelrico.com>
2020-04-07 07:04:51 -0700

    :sparkles: WeatherApp: cityService

--- part8_app/app.js
+++ part8_app/app.js
@@ -13,10 +13,17 @@ weatherApp.config(function($routeProvider, $locationProvider) {
     });
 });
 
-weatherApp.controller('homeController', ['$scope', function($scope) {
+weatherApp.service('cityService', function() {
+  this.city = "New York, NY";
+});
 
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+  $scope.city = cityService.city;
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  })
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function ($scope) {
-
+weatherApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService) {
+  $scope.city = cityService.city;
 }]);
