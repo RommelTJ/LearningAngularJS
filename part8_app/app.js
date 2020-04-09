a180b1b1527900667ad7cb3542b8b4f91622dd0a
Rommel Rico <me@rommelrico.com>
2020-04-09 06:58:29 -0700

    :truck: WeatherApp: Moving controllers to own file

--- part8_app/app.js
+++ part8_app/app.js
@@ -1,32 +1,6 @@
 var weatherApp = angular.module('weatherApp', ["ngRoute", "ngResource", "ngSanitize"]);
 
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
-  $scope.city = cityService.city;
-  $scope.$watch('city', function() {
-    cityService.city = $scope.city;
-  })
-}]);
 
-weatherApp.controller(
-  'forecastController',
-  ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
-    $scope.city = cityService.city;
-    $scope.days = $routeParams.days || '2';
-    $scope.weatherAPI = $resource(
-      "https://api.openweathermap.org/data/2.5/forecast",
-      { q: $scope.city, cnt: $scope.days, appid: "REDACTED" },
-      { 'query': { method: 'GET'} }
-    );
-    $scope.weatherResult = $scope.weatherAPI.query();
-    $scope.convertToCelsius = function(degK) {
-      return Math.round(degK - 273.15);
-    };
-    $scope.convertToDate = function(dt) {
-      // datetime from OpenWeatherMap comes in milliseconds
-      return new Date(dt * 1000);
-    };
-  }]
-);
 
 weatherApp.directive("weatherReport", function() {
   return {
