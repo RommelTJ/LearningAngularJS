b0715e838281a94085ae578a02a33dcc9a4d2949
Rommel Rico <me@rommelrico.com>
2020-04-09 06:59:24 -0700

    :truck: WeatherApp: Moving directives

--- part8_app/app.js
+++ part8_app/app.js
@@ -1,17 +1,1 @@
 var weatherApp = angular.module('weatherApp', ["ngRoute", "ngResource", "ngSanitize"]);
-
-
-
-weatherApp.directive("weatherReport", function() {
-  return {
-    restrict: 'E',
-    templateUrl: './directives/weatherReport.html',
-    replace: true,
-    scope: {
-      weatherDay: "=",
-      convertToStandard: "&",
-      convertToDate: "&",
-      dateFormat: "@"
-    }
-  }
-});
