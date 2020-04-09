97d39b6fd9a9d1f035b346b85934d03d5b4f3a53
Rommel Rico <me@rommelrico.com>
2020-04-09 06:53:29 -0700

    :sparkles: WeatherApp: Converting to custom directive

--- part8_app/app.js
+++ part8_app/app.js
@@ -56,3 +56,17 @@ weatherApp.controller(
     };
   }]
 );
+
+weatherApp.directive("weatherReport", function() {
+  return {
+    restrict: 'E',
+    templateUrl: './directives/weatherReport.html',
+    replace: true,
+    scope: {
+      weatherDay: "=",
+      convertToStandard: "&",
+      convertToDate: "&",
+      dateFormat: "@"
+    }
+  }
+});
