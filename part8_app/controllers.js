526fc442672e0072420a1b7702d0d55bcddffc2e
Rommel Rico <me@rommelrico.com>
2020-04-10 06:49:07 -0700

    :sparkles: Bonus: ng-submit

--- part8_app/controllers.js
+++ part8_app/controllers.js
@@ -1,9 +1,15 @@
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
-  $scope.city = cityService.city;
-  $scope.$watch('city', function() {
-    cityService.city = $scope.city;
-  })
-}]);
+weatherApp.controller(
+  'homeController',
+  ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
+    $scope.city = cityService.city;
+    $scope.$watch('city', function() {
+      cityService.city = $scope.city;
+    });
+    $scope.submit = function() {
+      $location.path('/forecast');
+    }
+  }]
+);
 
 weatherApp.controller(
   'forecastController',
