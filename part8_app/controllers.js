63b2d95770f19f8b4ad6b2607391703b3894c319
Rommel Rico <me@rommelrico.com>
2020-04-10 07:00:44 -0700

    :sparkles: Bonus: Designing services in large AngularJS applications

--- part8_app/controllers.js
+++ part8_app/controllers.js
@@ -13,21 +13,22 @@ weatherApp.controller(
 
 weatherApp.controller(
   'forecastController',
-  ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
-    $scope.city = cityService.city;
-    $scope.days = $routeParams.days || '2';
-    $scope.weatherAPI = $resource(
-      "https://api.openweathermap.org/data/2.5/forecast",
-      { q: $scope.city, cnt: $scope.days, appid: "REDACTED" },
-      { 'query': { method: 'GET'} }
-    );
-    $scope.weatherResult = $scope.weatherAPI.query();
-    $scope.convertToCelsius = function(degK) {
-      return Math.round(degK - 273.15);
-    };
-    $scope.convertToDate = function(dt) {
-      // datetime from OpenWeatherMap comes in milliseconds
-      return new Date(dt * 1000);
-    };
-  }]
+  [
+    '$scope',
+    '$routeParams',
+    'cityService',
+    'weatherService',
+    function ($scope, $routeParams, cityService, weatherService) {
+      $scope.city = cityService.city;
+      $scope.days = $routeParams.days || '2';
+      $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
+      $scope.convertToCelsius = function(degK) {
+        return Math.round(degK - 273.15);
+      };
+      $scope.convertToDate = function(dt) {
+        // datetime from OpenWeatherMap comes in milliseconds
+        return new Date(dt * 1000);
+      };
+    }
+  ]
 );
